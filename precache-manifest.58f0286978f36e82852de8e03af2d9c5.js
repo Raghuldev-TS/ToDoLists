c9a6b70f1ad0c4f7f38192d66d4cf64e8cc165f3
Raghuldev-TS <raghulsanjeev09@gmail.com>
2021-02-08 00:56:11 +0530

    Updates

---
+++ precache-manifest.58f0286978f36e82852de8e03af2d9c5.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b9769c95df8fe05b21fb4634eae5391",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "bba6d1fa93941e5b72e5",
+    "url": "/ToDoList/static/js/2.a57146ea.chunk.js"
+  },
+  {
+    "revision": "3f196c11c08d28ec148a",
+    "url": "/ToDoList/static/js/main.e3ce928a.chunk.js"
+  },
+  {
+    "revision": "83dcdf33ae0fa89a6a64",
+    "url": "/ToDoList/static/js/runtime-main.f0d39aaa.js"
+  }
+]);
